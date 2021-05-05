0025eb31728c48b9eafaa91bf79b932f34dd6572
Дмитрий Кушнарюк <dkushnaryuk@diasoft.ru>
2021-05-05 17:59:48 +0300

    01-init| work with flag

---
+++ cmd/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"flag"
+)
+
+// flag params
+var fromPath string
+var toPath string
+var limit int
+var offset int
+var procCsvPath string
+
+func init() {
+	flag.StringVar(&procCsvPath, "flu", "./FLUproc.csv", "path to the processing file")
+	flag.StringVar(&fromPath, "from", "", "the file to read from")
+	flag.StringVar(&toPath, "to", "", "the file to write")
+	flag.IntVar(&limit, "limit", 0, "limit of copied bytes")
+	flag.IntVar(&offset, "offset", 0, "offset of copied bytes")
+
+	flag.Parse()
+}
+
+func main() {
+	if procCsvPath != "" {
+
+	} else {
+
+	}
+}
