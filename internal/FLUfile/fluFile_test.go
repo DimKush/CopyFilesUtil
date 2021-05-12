c9defd0fa950c8422c932d700e9cae0cbda2cad7
Дмитрий Кушнарюк <dkushnaryuk@diasoft.ru>
2021-05-12 23:11:28 +0300

    04-units | new unit tests

--- internal/FLUfile/fluFile_test.go
+++ internal/FLUfile/fluFile_test.go
@@ -1,7 +1,34 @@
 package FLUfile
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
+
+func prepareContentFile() error {
+	file, err := os.OpenFile("FLUproc.csv", os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		return err
+	}
+
+	str := "from,to,offset,limit\n" +
+		"/home/dim/Desktop/Go/CopyFilesUtil/cmd/main.go,/home/dim/Desktop/trash/m.go,50,100\n" +
+		"/home/dim/Desktop/Go/CopyFilesUtil/cmd/main.go,/home/dim/Desktop/trash/FLUfile.go,,\n"
+
+	_, err = file.Write([]byte(str))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 
 func TestParseUnit(t *testing.T) {
+	err := prepareContentFile()
 
+	if err != nil {
+		t.Fatalf("Error in TestCheckFile(). CreateTestedFiles() returned an error : %s", err)
+	}
+
+	os.Remove("FLUproc.csv")
 }
